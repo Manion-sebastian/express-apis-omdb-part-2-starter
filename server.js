ee923062fc4f9f874c04d3c775cbb8ee608fd856
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-08 11:25:51 -0700

    functional db with faves

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const axios = require('axios')
+const db = require('./models')
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -35,9 +36,37 @@ app.get('/details/:id', (req, res) => {
   console.log(req.params.id)
   axios.get(`http://www.omdbapi.com/?i=${req.params.id}&apikey=${APIKEY}`)
     .then(response => {
+      console.log(response.data)
       res.render('detail.ejs', { movie: response.data })
     })
     .catch(console.log)
 })
+
+// Faves routes
+// get /faves -- read all faves and display them to the user
+
+app.get('/faves', async (req,res)=>{
+  try { const allFaves = await db.fave.findAll()
+    res.render('faves.ejs', {allFaves})
+
+  } catch(err) {
+    console.log(err)
+    res.send('Server error')
+  }
+})
+
+app.post('/faves', async (req,res) => {
+  try {
+    // add the new favorite to the db
+    await db.fave.create(req.body)
+    // redirect to the user's profile with their faves
+    res.redirect('/faves')
+  } catch(err) {
+    console.log(err)
+    res.send('server error')
+  }
+})
+
+// post /faves -- create new facs and redirect to /faves to display user faves
 
 app.listen(process.env.PORT || 3000);
